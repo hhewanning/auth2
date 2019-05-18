efeb76ce3707d40cca992b8ca65bf392700bd600
hhewanning@163.com <hhewanning@163.com>
2019-05-19 01:04:02 +0800

    添加js文件

---
+++ prod.env.js
@@ -0,0 +1,7 @@
+module.exports = {
+    NODE_ENV: '"production"',
+    BASE_API: process.env.BASE_API,
+    MATERIAL_API: process.env.BASE_API,
+    UPLOAD_API: process.env.BASE_API,
+    IMPORT_API: process.env.BASE_API
+}
